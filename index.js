6fa37da85d66fe0513aeeb8864f04b1e2b2b28e4
TrungNQ <trungnq@tadi.io>
2017-08-25 16:43:08 +0700

    Fix bug

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 var importFolder = function(folderName) {
   var imported = {};
 
-  fs.readdirSync(folderName + '/').forEach(function(file) {
+  fs.readdirSync(`${folderName}/`).forEach(function(file) {
     if (file.match(/\.js$/) !== null && file !== 'index.js') {
       var name = file.replace('.js', '');
-      imported[name] = require('./' + file);
+      imported[name] = require(`${folderName}/${file}`);
     }
   });
 };
